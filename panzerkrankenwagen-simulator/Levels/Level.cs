7da76e77c101f5e84ea74a1bb6048a93e4d3aac6
Anish Satish <133033051+Anish-Satish@users.noreply.github.com>
2026-09-20 07:02:35 -0400

    leys see
    yay

--- panzerkrankenwagen-simulator/Levels/Level.cs
+++ panzerkrankenwagen-simulator/Levels/Level.cs
@@ -4,6 +4,9 @@ using System;
 public partial class Level : Node3D
 {
 	// Called when the node enters the scene tree for the first time.
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 	Timer timer;
 	public override void _Ready()
 	{
@@ -20,5 +23,24 @@ public partial class Level : Node3D
 			GetTree().Paused = true;
 			GetTree().Root.AddChild(Main.getPauseScene);
 		}
+=======
+=======
+>>>>>>> parent of f50f0c7 (time t okill)
+=======
+>>>>>>> parent of f50f0c7 (time t okill)
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of f50f0c7 (time t okill)
+=======
+>>>>>>> parent of f50f0c7 (time t okill)
+=======
+>>>>>>> parent of f50f0c7 (time t okill)
 	}
 }
--- panzerkrankenwagen-simulator/UI/PauseMenue.cs
+++ panzerkrankenwagen-simulator/UI/PauseMenue.cs
@@ -1,12 +1,15 @@
 using Godot;
 using System;
 
-public partial class MainMenuButtons : Godot.VBoxContainer
+public partial class PauseMenue : Control
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 	}
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD:panzerkrankenwagen-simulator/UI/Main Menu/Button.cs
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
@@ -27,8 +30,26 @@ public partial class MainMenuButtons : Godot.VBoxContainer
 		GetTree().Root.AddChild(Main.getOptionsScene);
 		GetTree().Root.RemoveChild(Main.getMainScene);
 	}
+=======
+>>>>>>> parent of f50f0c7 (time t okill):panzerkrankenwagen-simulator/UI/PauseMenue.cs
+=======
+>>>>>>> parent of f50f0c7 (time t okill)
+=======
+>>>>>>> parent of f50f0c7 (time t okill)
 	public void _on_quit_pressed()
 	{
 		GetTree().Quit();
+	}
+	public void _on_resume_pressed()
+	{
+		GetTree().ChangeSceneToPacked(Main.getDriveScene);
+	}
+	public void _on_options_pressed()
+	{
+		GetTree().ChangeSceneToPacked(Main.getOptionsScene);
+	}
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
 	}
 }
